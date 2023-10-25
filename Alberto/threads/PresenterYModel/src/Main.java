2dfaf57c807c90c181878a49bde8a22ea21dcc3a
Bryan J <bryandavila520@gmail.com>
2023-10-25 10:08:41 +0200

    Android-main

---
+++ Alberto/threads/PresenterYModel/src/Main.java
@@ -0,0 +1,36 @@
+import interfaz.MyInterface;
+import interfaz.Presenter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    MyInterface miPresentador = new Presenter();
+
+    Thread thread = new Thread();
+    Scanner input = new Scanner(System.in);
+
+    int inputValue = 0;
+
+
+
+
+    while(true){
+        System.out.println("Simula un 0 - Correcto");
+        System.out.println("Simula un 1 - Incorrecto");
+        System.out.println("Dame un valor");
+
+        if (inputValue == 0) {
+            System.out.println("Success");
+            miPresentador.mostrarLoginCorrecto();
+
+        }else {
+            System.out.println("Failure");
+            miPresentador.mostrarLoginInCorrecto();
+        }
+    }
+
+}
---
+++ Alberto/threads/PresenterYModel/src/interfaz/Presenter.java
@@ -0,0 +1,13 @@
+package interfaz;
+
+public class Presenter implements MyInterface {
+    @Override
+    public void mostrarLoginCorrecto() {
+        System.out.println("Login Correcto");
+    }
+
+    @Override
+    public void mostrarLoginInCorrecto() {
+
+    }
+}
