909b3ce87c096caa25a4e87dd74f9f80aaf0d608
Bryan J <bryandavila520@gmail.com>
2023-10-25 09:13:22 +0200

    Hilos

--- Diego/untitled/src/Main.java
+++ Diego/untitled/src/Main.java
@@ -1,11 +1,26 @@
 
 public class Main {
-    public static void main(String[] args) {
-        Mi1erHilo hilo = new Mi1erHilo();
-        hilo.start();
+    public static void main(String[] args) throws InterruptedException {
+        Mi1erHilo hilo1 = new Mi1erHilo(1);
+        Mi1erHilo hilo2 = new Mi1erHilo(2);
+        Mi1erHilo hilo3 = new Mi1erHilo(3);
+        Mi1erHilo hilo4 = new Mi1erHilo(4);
+        Mi1erHilo hilo5 = new Mi1erHilo(5);
+
+        hilo1.start();
+        hilo1.sleep(100);
+        hilo2.start();
+        hilo2.sleep(80);
+        hilo3.start();
+        hilo3.sleep(60);
+        hilo4.start();
+        hilo4.sleep(40);
+        hilo5.start();
+        hilo5.sleep(20);
+
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
--- Diego/untitled/src/Mi1erHilo.java
+++ Diego/untitled/src/Mi1erHilo.java
@@ -1,6 +1,10 @@
 public class Mi1erHilo extends Thread{
+    private int numHilo;
+    Mi1erHilo(int numHilo){
+        this.numHilo = numHilo;
+    }
     public void run(){
-        System.out.println("Hola mundo!!! Soy el primer hilo");
+        System.out.println("Hola mundo!!! Soy el hilo" + this.numHilo);
 
     }
 }
