6dd9616d945c0719f21d2a49b3a99cca68bc149e
Bryan J <bryandavila520@gmail.com>
2023-11-06 13:13:01 +0100

    3ºSkillTerminada

--- Alberto/3SkillRecyclerView/app/src/main/java/com/example/a3skillrecyclerview/pages.java
+++ Alberto/3SkillRecyclerView/app/src/main/java/com/example/a3skillrecyclerview/pages.java
@@ -3,6 +3,7 @@ package com.example.a3skillrecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class pages extends AppCompatActivity {
 
@@ -12,5 +13,8 @@ public class pages extends AppCompatActivity {
         setContentView(R.layout.activity_pages);
 
         int pageId = getIntent().getIntExtra("id",0);
+
+        TextView TextPageId = findViewById(R.id.TextPageId);
+        TextPageId.setText("PAGE:" + pageId);
     }
 }
