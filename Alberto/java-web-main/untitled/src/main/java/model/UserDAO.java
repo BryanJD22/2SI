8aa02208d3e91b76eb9568c967e5e46b96d5fe8b
Bryan J <bryandavila520@gmail.com>
2023-11-20 20:14:36 +0100

    intento de intro

--- Alberto/java-web-main/untitled/src/main/java/model/UserDAO.java
+++ Alberto/java-web-main/untitled/src/main/java/model/UserDAO.java
@@ -41,7 +41,7 @@ public class UserDAO implements DAO<User, Integer> {
             while (rs.next()) {
                 User user = new User(rs.getString("USERNAME"),
                         rs.getString("EMAIL"),
-                        rs.getString("PASSWORD"));
+                        rs.getString("PASS"));
 
                 usuarios.add(user);
             }
