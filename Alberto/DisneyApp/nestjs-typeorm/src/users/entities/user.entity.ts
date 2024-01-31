7fad48ab5bcf41084a624984f2191cace6720626
Bryan J <bryandavila520@gmail.com>
2024-01-31 12:27:33 +0100

    cosas de programación

--- Alberto/DisneyApp/nestjs-typeorm/src/users/entities/user.entity.ts
+++ Alberto/DisneyApp/nestjs-typeorm/src/users/entities/user.entity.ts
@@ -1,41 +1,7 @@
-import {
-  Entity,
-  Column,
-  PrimaryColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
-  OneToOne,
-} from 'typeorm';
-
-import { User } from './user.entity';
-
-@Entity()
-export class Customer {
-  @PrimaryColumn()
+export class User {
   id: number;
-
-  @Column({ type: 'varchar', length: 255 })
-  name: string;
-
-  @Column({ type: 'varchar', length: 255 })
-  lastName: string;
-
-  @Column({ type: 'varchar', length: 255 })
-  phone: string;
-
-  @CreateDateColumn({
-    type: 'timestamptz',
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  createAt: Date;
-
-  @UpdateDateColumn({
-    type: 'timestamptz',
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  updateAt: Date;
-
-  @OneToOne(() => User, (user) => user.customer, { nullable: true })
-  user: User; // cliente pasa por el carrito pero no es usuario, no hizo compra, no se registró
-// Decorador Join Column sólo en un lado, la que carga con la relación sólo es 1. No duplicar. Sólo 1 el decorador
+  email: string;
+  password: string;
+  role: string;
+  customer: any;
 }
